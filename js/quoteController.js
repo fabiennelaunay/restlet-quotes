5dbaf855d4fc66182f16c824833b7947efeaf6d7
fabiennelaunay <flaunay@restlet.com>
2015-11-10 16:46:22 +0100

    Updated API URL content

--- js/quoteController.js
+++ js/quoteController.js
@@ -28,7 +28,7 @@ angular.module('quotesApp')
     ctrl.QUOTE_LS = 'quoteSelection';
     ctrl.KEY_LS = 'keySelection';
 
-    var myEventSource = streamdataio.createEventSource('Edit APISpark API URL', STREAMDATAIO_KEY);
+    var myEventSource = streamdataio.createEventSource('https://myquotes.apispark.net/v1/quotes/', STREAMDATAIO_KEY);
 
     myEventSource
       .onData(ctrl.onData)
